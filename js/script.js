bde7b8dd96ed637abdc45299db8fe2789f0d8c5b
cnnrbrn <connorbobrien@gmail.com>
2020-02-04 01:26:51 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Question 1
+const multiply = (a, b) => a * b;
+
+
+// Question 2
+() => console.log("Hello");
+
+
+// Question 3
+const frog = {
+    colour: "ss",
+    getColour: function() {
+        return this.colour;
+    }
+}
+
+// Question 4
+(function() {
+    console.log(new Date());
+})();
