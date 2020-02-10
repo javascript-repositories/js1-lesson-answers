cceea14643d00437b92ed43bf92426792a80d14c
cnnrbrn <connorbobrien@gmail.com>
2020-02-10 14:28:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,33 @@
-const cars = [
-    {
-        type: "BMW",
-        age: 4,
-        beenInAnAccident: false
-    },
-    {
-        type: "Toyota",
-        beenInAnAccident: true
-    },
-    {
-        type: "Ford",
-        age: 7,
-        beenInAnAccident: true
-    }
+const elephants = [
+    { image: "https://elephant-api.herokuapp.com/pictures/001.jpg", note: "Given to a Carolingian emperor." },
+    { image: "https://elephant-api.herokuapp.com/pictures/missing.jpg", note: "From the Mysore Dasara procession." },
+    { image: "https://elephant-api.herokuapp.com/pictures/missing.jpg", note: "Known for her tightrope walking act." }
 ];
 
-// using a for loop
-for (let i = 0; i < cars.length; i++) {
-    if (cars[i].age) {
-        console.log(cars[i].age);
-    } else {
-        console.log("Unkown");
+// for loop
+function createHtmlWithFor(images) {
+    let html = "";
+
+    for (let i = 0; i < images.length; i++) {
+        html += `<img src="${images[i].image}" alt="${images[i].note}">`;
     }
+
+    return html;
 }
 
-// using a forEach
-cars.forEach(car => {
-    if (car.age) {
-        console.log(car.age);
-    } else {
-        console.log("Unkown");
-    }
-});
+const html1 = createHtmlWithFor(elephants);
+console.log(html1);
 
-// using a forEach and a ternary operator
-// https://interactive-content.now.sh/javascript-1/3/1#ternary
-cars.forEach(car => {
-    const age = car.age ? car.age : "Unknown";
-    console.log(age);
-});
+// forEach loop
+function createHtmlWithForEach(images) {
+    let html = "";
 
-// using a forEach and the hasOwnProperty method
-// we haven't looked at the hasOwnProperty but it checks whether a property exists on an object
-cars.forEach(car => {
-    if (car.hasOwnProperty("age")) {
-        console.log(car.age);
-    } else {
-        console.log("Unkown");
-    }
-});
+    images.forEach(img => {
+        html += `<img src="${img.image}" alt="${img.note}">`;
+    });
+
+    return html;
+}
+
+const html2 = createHtmlWithForEach(elephants);
+console.log(html2);
