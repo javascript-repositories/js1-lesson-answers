4d656d88d09660070e2985ba08b967e00190832c
cnnrbrn <connorbobrien@gmail.com>
2020-02-10 15:36:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 const elephants = [
-    { image: "https://elephant-api.herokuapp.com/pictures/001.jpg", note: "Given to a Carolingian emperor." },
-    { image: "https://elephant-api.herokuapp.com/pictures/missing.jpg", note: "From the Mysore Dasara procession." },
-    { image: "https://elephant-api.herokuapp.com/pictures/missing.jpg", note: "Known for her tightrope walking act." }
+    { image: "https://elephant-api.herokuapp.com/pictures/001.jpg", note: null },
+    { note: "From the Mysore Dasara procession." },
+    { image: null, note: "Known for her tightrope walking act." }
 ];
 
 // for loop
 function createHtmlWithFor(images) {
-    let html = "";
+    // add the opening </div>
+    let html = "<div>";
 
     for (let i = 0; i < images.length; i++) {
-        html += `<img src="${images[i].image}" alt="${images[i].note}">`;
+        // default image url
+        let imgUrl = "https://via.placeholder.com/250";
+
+        if (images[i].image) {
+            imgUrl = images[i].image;
+        }
+
+        // default alt value
+        let altValue = "Elephant picture";
+
+        if (images[i].note) {
+            altValue = images[i].note;
+        }
+
+        html += `<img src="${imgUrl}" alt="${altValue}">`;
     }
+
+    // add the closing </div>
+    html += "</div>";
 
     return html;
 }
@@ -18,16 +36,44 @@ function createHtmlWithFor(images) {
 const html1 = createHtmlWithFor(elephants);
 console.log(html1);
 
-// forEach loop
-function createHtmlWithForEach(images) {
-    let html = "";
+// for loop with ternary operators for the default values
+function createHtmlWithTernary(images) {
+    // add the opening </div>
+    let html = "<div>";
 
-    images.forEach(img => {
-        html += `<img src="${img.image}" alt="${img.note}">`;
-    });
+    for (let i = 0; i < images.length; i++) {
+        // default image url
+        let imgUrl = images[i].image ? images[i].image : "https://via.placeholder.com/250";
+        // default alt value
+        let altValue = images[i].note ? images[i].note : "Elephant picture";
+
+        html += `<img src="${imgUrl}" alt="${altValue}">`;
+    }
+
+    // add the closing </div>
+    html += "</div>";
 
     return html;
 }
 
-const html2 = createHtmlWithForEach(elephants);
+const html2 = createHtmlWithTernary(elephants);
 console.log(html2);
+
+// forEach loop
+function createHtmlWithForEach(images) {
+    let html = "<div>";
+
+    images.forEach(img => {
+        let imgUrl = img.image ? img.image : "https://via.placeholder.com/250";
+        // default alt value
+        let altValue = img.note ? img.note : "Elephant picture";
+
+        html += `<img src="${imgUrl}" alt="${altValue}">`;
+    });
+
+    html += "</div>";
+    return html;
+}
+
+const html3 = createHtmlWithForEach(elephants);
+console.log(html3);
